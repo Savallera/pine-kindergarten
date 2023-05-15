ac3cc7bbecd0eadf4c3c69f8084d1c73ccbd4a11
Navat2007 <navat2007@yandex.ru>
2023-05-15 15:20:46 +0300

    add

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,12 +1,41 @@
 // Бургерное меню
+function openDropMenu(e) {
+
+    e.stopPropagation();
+
+    if(burgerButton.classList.contains("burger_opened")) {
+        closeDropMenu();
+    }
+    else {
+        burgerButton.classList.add("burger_opened");
+        menuContainer.classList.add("menu_opened");
+
+        isMenuOpened = true;
+    }
+
+}
+
+function closeDropMenu() {
+    burgerButton.classList.remove("burger_opened");
+    menuContainer.classList.remove("menu_opened");
+
+    isMenuOpened = false;
+}
+
 const burgerButton = document.querySelector(".burger");
 const menuContainer = document.querySelector(".menu_place_header");
-const toggleMenu = () => {
-    burgerButton.classList.toggle("burger_opened");
-    menuContainer.classList.toggle("menu_opened");
-};
+let isMenuOpened = false;
 
-burgerButton.addEventListener("click", toggleMenu);
+burgerButton.addEventListener("click", openDropMenu);
+document.addEventListener('click', (event) => {
+
+    if (event.target.closest(".menu__inner") === null && isMenuOpened === true) {
+
+        closeDropMenu();
+
+    }
+
+});
 
 // Липкий хедер
 let scrollPos = window.scrollY;
